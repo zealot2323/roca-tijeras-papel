403850b68d52992aa7b1ca343f52fb084c460bc4
Eric Pratt <zealot2323@gmail.com>
2024-12-11 21:16:19 +0100

    Added logic to determine winner and increment score for single round

--- roca.js
+++ roca.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let compScore = 0;
+
 function getComputerResponse() {
     let ran = Math.random();
     if (ran < .333) {
@@ -14,5 +17,42 @@ function getHumanChoice() {
     return humanResponse.toLowerCase()
 }
 
-console.log(getComputerResponse());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Tie!")
+    } else if (humanChoice === "roca") {
+        if (computerChoice === "tijeras") {
+            humanScore ++;
+            console.log("Human wins with Roca beating tijeras!");
+        } else {
+            compScore++;
+            console.log("Comp wins with Paper beating roca!");
+        }
+    } else if (humanChoice === "tijeras") {
+        if (computerChoice === "papel") {
+            humanScore ++;
+            console.log("Human wins with Tijeras beating papel!");
+        } else {
+            compScore++;
+            console.log("Comp wins with Roca beating Tijeras!");
+        }
+    } else if (humanChoice === "papel") {
+        if (computerChoice === "roca") {
+            humanScore ++;
+            console.log("Human wins with Papel beating roca!");
+        } else {
+            compScore++;
+            console.log("Comp wins with Tijeras beating papel!");
+        }
+    }
+  }
+  
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerResponse();
+  
+playRound(humanSelection, computerSelection);
+
+console.log(humanSelection);
+console.log(computerSelection);
+console.log(humanScore);
+console.log(compScore);
