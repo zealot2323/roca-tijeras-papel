5d89ea12ecf88cdd2525e3a7b2d22f4cb4768b5f
Eric Pratt <zealot2323@gmail.com>
2024-12-17 19:28:17 +0100

    Updated to add UI

--- roca.js
+++ roca.js
@@ -1,3 +1,25 @@
+const buttons = document.querySelector("#buttons");
+
+buttons.addEventListener("click", (event) => {
+  let target = event.target;
+
+  switch(target.id) {
+      case "roca":
+            playRound("roca", getComputerResponse());
+            break;
+      case "tijeras":
+            playRound("tijeras", getComputerResponse());
+            break;
+      case "papel":
+            playRound("papel", getComputerResponse());
+            break;
+  }
+});
+
+const scorebox = document.querySelector("#score");
+const scoreHelper = document.createElement("p");
+const score = document.createElement("h3");
+
 let humanScore = 0;
 let compScore = 0;
 
@@ -10,59 +32,59 @@ function getComputerResponse() {
     } else {
         return "papel"
     }
-}
-
-function getHumanChoice() {
-    let humanResponse = prompt("Entre roca, tijeras, o papel.", "roca");
-    return humanResponse.toLowerCase()
-}
+};
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log("Tie!")
+        scoreHelper.textContent = "Tie!";
+        console.log("Tie!");
     } else if (humanChoice === "roca") {
         if (computerChoice === "tijeras") {
             humanScore ++;
-            console.log("Human wins with Roca beating tijeras!");
+            updateScore("Human wins with Roca beating tijeras!");
         } else {
             compScore++;
-            console.log("Comp wins with Paper beating roca!");
+            updateScore("Comp wins with Paper beating roca!");
         }
     } else if (humanChoice === "tijeras") {
         if (computerChoice === "papel") {
             humanScore ++;
-            console.log("Human wins with Tijeras beating papel!");
+            updateScore("Human wins with Tijeras beating papel!");
         } else {
             compScore++;
-            console.log("Comp wins with Roca beating Tijeras!");
+            updateScore("Comp wins with Roca beating Tijeras!");
         }
     } else if (humanChoice === "papel") {
         if (computerChoice === "roca") {
             humanScore ++;
-            console.log("Human wins with Papel beating roca!");
+            updateScore("Human wins with Papel beating roca!");
         } else {
             compScore++;
-            console.log("Comp wins with Tijeras beating papel!");
+            updateScore("Human wins with Papel beating roca!");
         }
     }
-  }
+  };
 
-  function playGame () {
-    for (let i = 0; i < 5; i++) { 
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerResponse();
-  
-        playRound(humanSelection, computerSelection);
-    }
+  function updateScore(message) {
+    score.textContent = `Human: ${humanScore} - Computer: ${compScore}`;
+    scorebox.appendChild(score);
+    checkScore(message);
+    scorebox.appendChild(scoreHelper);
+    
+  };
 
-    let winner = "";
-    if (compScore === humanScore) {
-        return "Tie!"
-    } else if (compScore > humanScore) {
-        return `Computer wins with ${compScore} to ${humanScore}`
-    } else {
-        return `Human wins with ${humanScore} to ${compScore}`
-    }
-  }
-
-console.log(playGame());
+  function checkScore(message) {
+        if(humanScore === 5 || compScore === 5) {
+            if (compScore > humanScore) {
+                scoreHelper.textContent = `Computer wins with ${compScore} to ${humanScore}`;
+                humanScore = 0;
+                compScore = 0;
+            } else {
+                scoreHelper.textContent = `Human wins with ${humanScore} to ${compScore}`;
+                humanScore = 0;
+                compScore = 0;
+            }
+        } else {
+            scoreHelper.textContent = message;
+        }
+  };
